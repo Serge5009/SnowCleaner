5bf4e958864fc2a141ea227c48ae62de081d568a
Serhii Marchenko <serge5709244@gmail.com>
2022-11-26 18:29:13 -0500

    Player moves in a proper direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float speed = 5.0f;
+    public float maxSpeed = 5.0f;
+    public float maxTurnSpeed = 50.0f;
 
-
+    float speed = 0.0f;
+    float turnSpeed = 0.0f;
 
     void Start()
     {
-        
+        speed = maxSpeed;
+        turnSpeed = maxTurnSpeed;
     }
 
     void Update()
     {
-        transform.position += Vector3.forward * Time.deltaTime * speed;
         if (Input.GetKey("a"))
         {
-            transform.Rotate(new Vector3(0, -1, 0));
+            transform.Rotate(new Vector3(0, -1 * turnSpeed * Time.deltaTime, 0));
         }
-        else if (Input.GetKey("d"))
+        if (Input.GetKey("d"))
         {
-            transform.Rotate(new Vector3(0, 1, 0));
+            transform.Rotate(new Vector3(0, 1 * turnSpeed * Time.deltaTime, 0));
         }
+
+        transform.position += transform.forward * Time.deltaTime * speed;
     }
 }
