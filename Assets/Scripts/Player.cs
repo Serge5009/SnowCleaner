f3cb29ee3041b02c83a2a68d4084d5d259360fc0
Serhii Marchenko <serge5709244@gmail.com>
2022-11-26 18:34:50 -0500

    Added acceleration

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,26 +6,41 @@ public class Player : MonoBehaviour
 {
     public float maxSpeed = 5.0f;
     public float maxTurnSpeed = 50.0f;
+    public float acceleration = 5.0f;
 
     float speed = 0.0f;
     float turnSpeed = 0.0f;
 
     void Start()
     {
-        speed = maxSpeed;
+        speed = 0.0f;
         turnSpeed = maxTurnSpeed;
     }
 
     void Update()
     {
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a"))  //  Right turn input
         {
             transform.Rotate(new Vector3(0, -1 * turnSpeed * Time.deltaTime, 0));
         }
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d"))  //  Left turn input
         {
             transform.Rotate(new Vector3(0, 1 * turnSpeed * Time.deltaTime, 0));
         }
+
+        if (Input.GetKey("a") && Input.GetKey("d"))
+        {
+            speed -= acceleration / 2 * Time.deltaTime;
+        }
+        else
+        {
+            speed += acceleration * Time.deltaTime;
+        }
+
+        if (speed > maxSpeed)
+            speed = maxSpeed;
+        if (speed < maxSpeed / -2)
+            speed = maxSpeed / -2;
 
         transform.position += transform.forward * Time.deltaTime * speed;
     }
